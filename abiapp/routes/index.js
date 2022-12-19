9162068780b1a227cfe10820fec214ec07ce76eb
AbiMA <48912566+indigosaurus@users.noreply.github.com>
2022-12-19 19:02:25 +0000

    adding cors

--- abiapp/routes/index.js
+++ abiapp/routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Hellsite (affectionate)' });
-  res.redirect('/catalog'); //reroutes to path/catalog
+router.use(cors());
+
+// GET home page.
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
 });
 
 module.exports = router;
